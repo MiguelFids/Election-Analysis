587c58c0342f379b1cb0731760255d769ed95556
MiguelFids <miguel.alfonso.fidelino@gmail.com>
2022-09-10 11:10:46 -0400

    Finalize the Election Results COMPLETED

--- PyPoll.py
+++ PyPoll.py
@@ -32,31 +32,40 @@ with open(file_to_load) as election_data:
             candidate_votes[candidate_name] = 0
         candidate_votes[candidate_name] += 1
 
+with open(file_to_save, "w") as txt_file:
+# Print the final vote count to the terminal.
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+    
+    # Save the final vote count to the text file.
+    txt_file.write(election_results)
+
     for candidate_name in candidate_votes:
         # Retrieve vote count of a candidate.
         votes = candidate_votes[candidate_name]
 
         vote_percentage = float(votes) / float(total_votes) * 100
+       
+        # print()
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        txt_file.write(candidate_results)
 
         if (votes > winning_count) and (vote_percentage > winning_percentage):
 
             winning_count = votes
             winning_percentage = vote_percentage
-
             winning_candidate = candidate_name
         
-        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+    txt_file.write(winning_candidate_summary)        
 
-
-# print the winner
-
-winning_candidate_summary = (
-f"-------------------------\n"
-f"Winner: {winning_candidate}\n"
-f"Winning Vote Count: {winning_count:,}\n"
-f"Winning Percentage: {winning_percentage:.1f}%\n"
-f"-------------------------\n")
-print(winning_candidate_summary)
-
-            
 
